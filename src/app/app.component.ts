8d1f8e81bc747e88374520f0c209decae79fd264
swaroop1491 <snaik303@gmail.com>
2020-10-16 12:37:05 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,61 +1,55 @@
-import { Component, VERSION, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, VERSION, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  constructor(private http: HttpClient) { }
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  constructor(private http: HttpClient) {}
+  name = "Angular " + VERSION.major;
   currentSelection = "Trending";
   favValue = "";
   favList = [];
   topicData: any = [];
   html: any = "";
-  ngOnInit(){
+  ngOnInit() {
     this.favList = JSON.parse(localStorage.getItem("favList"));
-    this.http.get("https://kkapi.swaroop1491.repl.co/news/India+Trending").subscribe(resp=>{
-      this.topicData = resp;
-    });
+    this.http
+      .get("https://kkapi.swaroop1491.repl.co/news/India+Trending")
+      .subscribe(resp => {
+        this.topicData = resp;
+      });
   }
   arr: any = [
-    {"topic": "Trending", "active":true},
-    {"topic": "My Fav", "active":false},
-    {"topic": "Corona Virus", "active":false},
-    {"topic": "IPL", "active":false},
-    {"topic": "Odisha", "active":false},
-    {"topic": "India", "active":false},
-    {"topic": "International", "active":false},
-    {"topic": "Business", "active":false},
-    {"topic": "Sports", "active":false},
-    {"topic": "Entertainment", "active":false},
-    {"topic": "Science", "active":false},
-    {"topic": "Astrology", "active":false}
-    ]
-  topicClk(e){
-    console.log("click topic")
+    { topic: "Trending", active: true },
+    { topic: "My Fav", active: false },
+    { topic: "Corona Virus", active: false },
+    { topic: "IPL", active: false }
+  ];
+  topicClk(e) {
+    console.log("click topic");
     this.currentSelection = e.topic;
-    for(let i=0;i<this.arr.length;i++){
-      if(this.arr[i].topic == e.topic) this.arr[i].active = true;
-      else  this.arr[i].active = false;
-    };
-    this.http.get("https://kkapi.swaroop1491.repl.co/news/"+e.topic).subscribe(resp=>{
-      this.topicData = resp;
-    });
+    for (let i = 0; i < this.arr.length; i++) {
+      if (this.arr[i].topic == e.topic) this.arr[i].active = true;
+      else this.arr[i].active = false;
+    }
+    this.http
+      .get("https://kkapi.swaroop1491.repl.co/news/" + e.topic)
+      .subscribe(resp => {
+        this.topicData = resp;
+      });
   }
-  favtopicClk(e){
-
-  }
-  addFav(){
-    if(!this.favValue) alert("Please type some topic name");
+  favtopicClk(e) {}
+  addFav() {
+    if (!this.favValue) alert("Please type some topic name");
     else this.favList.push(this.favValue);
     localStorage.setItem("favList", JSON.stringify(this.favList));
-    this.favValue="";
+    this.favValue = "";
   }
-  removeFavItem(e){
+  removeFavItem(e) {
     let i = this.favList.indexOf(e);
-    this.favList.splice(i,1);
+    this.favList.splice(i, 1);
     localStorage.setItem("favList", JSON.stringify(this.favList));
   }
 }
